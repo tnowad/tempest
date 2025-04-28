230a19d376e9790d68164bd131ee8b2d92150cd5
Nguyen Minh Tuan <tnowad@gmail.com>
2025-04-29 05:56:56 +0700

    feat: add check permission

--- app/src/main/java/com/tnowad/tempest/MainActivity.java
+++ app/src/main/java/com/tnowad/tempest/MainActivity.java
@@ -1,24 +1,47 @@
 package com.tnowad.tempest;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<String> permissionRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        permissionRequest = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                // Location permission granted, proceed
+                startHomeActivity();
+            } else {
+                // Permission denied, show a message or handle accordingly
+                Toast.makeText(this, "Location permission required", Toast.LENGTH_SHORT).show();
+            }
         });
+
+        // Check for permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        } else {
+            startHomeActivity();
+        }
+    }
+
+    private void startHomeActivity() {
+        // Proceed to Home Activity if permission is granted
+        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+        finish();
     }
 }
